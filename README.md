ada6f9e67b56d1070d3112b5452d5a68d5495243
Wisaac <l.isaac.taveraz@gmail.com>
2024-04-07 13:36:45 -0600

    Finished Buttons, dark mode compatible

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Tailwind css custom clases like Bootstrap
-Making some custom clases with `@layer` and `@apply`, to just like bootstrap make fast controls and components.
 
-Work in progress...
+In this project I'm trying to recreate "bootstrap like" elements, with some custom css clases with `@layer` and `@apply` by tailwind, to help making elements and controls faster than with vanilla tailwind with **only one simple css file**.
 
-![alt text](screenshot_1.png)
+*Work in progress...*
+
+### Finished Work
+
+Body General Classes, Buttons.
+
+## Simple Text Layout
+
+![alt text](screenshots/image-0.png)
+
+## Simple Buttons
+
+![alt text](screenshots/image-1.png)
+
+## Simple Buttons, Dark Mode compatible
+
+![alt text](screenshots/image-2.png)
 
 ## Available Scripts
 
-### `yarn start`
-### `yarn test`
-### `yarn build`
+`yarn start` `yarn test` `yarn build`
 
 ## Learn More
 
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import ButtonsSection from "./components/ButtonsSection";
+import TextSection from "./components/TextSection";
 
 function App() {
   return (
@@ -9,46 +11,10 @@ function App() {
           <div className="-m-1.5 overflow-x-auto">
             <div className="p-1.5 min-w-full inline-flex flex-col gap-3 align-middle">
               {/* Text and Layout */}
-              <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
-                <div className="m-5">
-                  <h1>Header</h1>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi natus unde tenetur magni odio cumque non explicabo, eligendi laboriosam aliquid voluptate doloremque est pariatur nulla commodi nobis, voluptatibus amet? Ea.</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquam magni amet. Earum recusandae, quasi nesciunt illum tempora reprehenderit velit ab harum inventore quia ea veniam molestiae commodi! Cum, facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor corrupti totam quod praesentium aliquid quasi reiciendis sint, eveniet accusamus cumque exercitationem autem facilis necessitatibus, facere sed? Ullam vel eaque aperiam!</p>
-                  <h1>Header 1</h1>
-                  <h2>Header 2</h2>
-                  <h3>Header 3</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque corrupti quo unde, id facere perferendis accusantium voluptatibus nihil suscipit, temporibus rem repellat dicta necessitatibus sint non, nisi illo officia enim.</p>
-                </div>
-              </div>
+              <TextSection />
               {/* Buttons */}
-              <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
-                <div className="m-5 flex flex-col gap-2">
-                  <h1>Buttons</h1>
-                  <div className="inline-flex flex-wrap gap-2">
-                    <button className="btn" href="#">Primary</button>
-                    <button className="btn btn-outline" href="#">Primary</button>
-                    <button className="btn btn-secondary" href="#">Secondary</button>
-                    <button className="btn btn-outline-secondary" href="#">Secondary</button>
-                    <button className="btn btn-danger" href="#">Danger</button>
-                    <button className="btn btn-outline-danger" href="#">Danger</button>
-                    <button className="btn btn-dark" href="#">Dark</button>
-                    <button className="btn btn-outline-dark" href="#">Dark</button>
-                    <button className="btn btn-link" href="#">Link</button>
-                  </div>
-                  <h1>Disabled Buttons</h1>
-                  <div className="inline-flex flex-wrap gap-2">
-                    <button className="btn" href="#" disabled>Primary</button>
-                    <button className="btn btn-outline" href="#" disabled>Primary</button>
-                    <button className="btn btn-secondary" href="#" disabled>Secondary</button>
-                    <button className="btn btn-outline-secondary" href="#" disabled>Secondary</button>
-                    <button className="btn btn-danger" href="#" disabled>Danger</button>
-                    <button className="btn btn-outline-danger" href="#" disabled>Danger</button>
-                    <button className="btn btn-dark" href="#" disabled>Dark</button>
-                    <button className="btn btn-outline-dark" href="#" disabled>Dark</button>
-                    <button className="btn btn-link" href="#" disabled>Link</button>
-                  </div>
-                </div>
-              </div>
+              <ButtonsSection />
+              <ButtonsSection darkTheme={true}/>
               {/* End Buttons */}
             </div>
           </div>
